8668252ac9d21ad1f31ce63a2e2ad02042d8a487
Do Mac <rittikiert.san@gmail.com>
2025-07-25 23:08:00 +0700

    update

--- src/middlewares/authenticate.middleware.js
+++ src/middlewares/authenticate.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-import { PrismaClient } from "@prisma/client";
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient(); // import Prisma client
 
 export const authenticate = async (req, res, next) => {
